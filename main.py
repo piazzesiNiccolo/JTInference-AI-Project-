07b3ff45fb3a50645d75b97ed43686fcc05f74a2
Niccolò Piazzesi <npiazzesi@gmail.com>
2020-03-05 11:28:28 +0100

    added icy road example

--- main.py
+++ main.py
@@ -4,21 +4,13 @@ from JoinTree import JunctionTree
 from pgmpy.factors.discrete.CPD import TabularCPD,DiscreteFactor
 import numpy as np
 
+##WET GRASS
 r = TabularCPD("r",2,[[0.8],[0.2]])
 s = TabularCPD("s",2,[[0.9],[0.1]])
 w = TabularCPD('w',2,[[0.8,0],[0.2,1]],['r'],[2])
 h = TabularCPD('h', 2,[[1,0.1,0,0],[0,0.9,1,1]],['r', 's'], [2, 2])
-print(r)
-print(s)
-print(w)
-print(h)
-ff = TabularCPD('h',2,[[0],[1]])
-ff2 = TabularCPD('w',2,[[0],[1]])
-print(ff)
 wr = Clique([w,r])
 hrs = Clique([h,r,s])
-wr.init_cpt()
-hrs.init_cpt()
 print(hrs.cpt.marginalize(['r','h'],False))
 jt = JunctionTree('f',wr,[wr,hrs])
 jt.add_separator(wr,hrs)
@@ -29,3 +21,20 @@ jt.enter_evidence('w',1)
 print(hrs.cpt.marginalize(['r','h'],False))
 
 
+##ICY ROAD
+
+icy = TabularCPD('i',2,[[0.3],[0.7]])
+holmes = TabularCPD('h',2,[[0.9,0.2],[0.1,0.8]],['i'],[2])
+watson = TabularCPD('w',2,[[0.9,0.2],[0.1,0.8]],['i'],[2])
+print(icy)
+print(holmes)
+print(watson)
+
+iw = Clique([icy,watson])
+ih = Clique([icy,holmes])
+
+jt2 = JunctionTree('t',iw,[iw,ih])
+jt2.add_separator(iw,ih)
+jt2.enter_evidence('w',0)
+print(ih.cpt.marginalize(['h'],False))
+print(iw.cpt.marginalize(['w'],False))
