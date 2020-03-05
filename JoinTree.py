b350833c5d67509deb68e051a9d25f66a06af10c
Niccolò Piazzesi <npiazzesi@gmail.com>
2020-03-05 11:07:51 +0100

    first test ok,need improvement

---
+++ JoinTree.py
@@ -0,0 +1,75 @@
+import numpy as np
+import copy
+from bayes import Clique, Separator
+from pgmpy.factors.discrete.CPD import TabularCPD
+
+
+class JunctionTree(object):
+
+    def __init__(self, name, root, list_cliques, separators=set()):
+        self.name = name
+        self.cliques = set(list_cliques)
+        self.separators = separators
+        self.root = root
+        self.initialize = False
+
+    def add_separator(self,clique1,clique2):
+        sep = Separator(clique1,clique2)
+        sep.init_cpt()
+        self.separators.add(sep)
+        clique1.add_neighbor(clique2,sep)
+    
+    
+    def enter_evidence(self, variable, value):
+        ev = TabularCPD(variable,2,[[0],[0]])
+        ev.get_values()[value] = 1 
+        for cl in self.cliques:
+            for node in cl.nodes:
+                if node.variable == variable:
+                    cl.cpt.product(ev)
+                    break
+        
+        for sep in self.separators:
+            for node in sep.nodes:
+                if node.variable == variable:
+                    sep.cpt.product(ev)
+                    break
+        self.propagate()
+
+    def propagate(self):
+        self.set_visited()
+        self.collect(None, self.root, None, True)
+        self.set_visited()
+        self.distribute(self.root)
+        for cl in self.cliques:
+            cl.cpt.normalize()
+        for sep in self.separators:
+            sep.cpt.normalize()
+
+    def collect(self, pc, cc, sepset, send):
+        print('collect')
+        cc.visited = True
+        for neighbor, sep in cc.neighbors:
+            if not neighbor.visited:
+                self.collect(cc, neighbor, sep, False)
+        if not send:
+            self.pass_message(cc, pc, sepset)
+
+    def distribute(self, clique):
+        print('distribute')
+        clique.visited = True
+        for neighbor, sep in clique.neighbors:
+            if not neighbor.visited:
+                self.pass_message(clique, neighbor, sep)
+                self.distribute(neighbor)
+
+    def pass_message(self, from_clique: Clique, to_clique: Clique, sep: Separator):
+        x = [node.variable for node in from_clique.nodes.difference(sep.nodes)]
+        new = from_clique.cpt.marginalize(x, False)
+        to_clique.cpt.product(new)
+        to_clique.cpt.divide(sep.cpt)
+        sep.cpt = new
+
+    def set_visited(self, visit=False):
+        for c in self.cliques:
+            c.visited = visit
--- main.py
+++ main.py
@@ -1,55 +1,31 @@
-from pybbn.graph.dag import Bbn
-from pybbn.graph.edge import Edge,EdgeType
-from pybbn.graph.jointree import EvidenceBuilder
-from pybbn.graph.potential import Potential,PotentialUtil
-from pybbn.graph.node import BbnNode
-from pybbn.graph.variable import Variable
-from pybbn.pptc.inferencecontroller import InferenceController
-from inference import JTNode
 
-a = BbnNode(Variable(0,'A',['ON','OFF']),[0.5,0.5])
-b = BbnNode(Variable(1,'B',['ON','OFF']),[0.5,0.5,0.4,0.6])
-c = BbnNode(Variable(2,'C',['ON','OFF']),[0.7,0.3,0.2,0.8])
-d = BbnNode(Variable(3,'D',['ON','OFF']),[0.9,0.1,0.5,0.5])
-e = BbnNode(Variable(4,'E',['ON','OFF']),[0.3,0.7,0.6,0.4])
-f = BbnNode(Variable(5,'F',['ON','OFF']),[0.01,0.99,0.01,0.99,0.01,0.99,0.99,0.01])
-g = BbnNode(Variable(6,'G',['ON','OFF']),[0.8,0.2,0.1,0.9])
-h = BbnNode(Variable(7,'H',['ON','OFF']),[0.5,0.95,0.95,0.5,0.95,0.05,0.95,0.05])
-bbn= Bbn()
-bbn = bbn\
-    .add_node(a)\
-    .add_node(b)\
-    .add_node(c)\
-    .add_node(d)\
-    .add_node(e)\
-    .add_node(f)\
-    .add_node(g)\
-    .add_node(h)\
-    .add_edge(Edge(a,b,EdgeType.DIRECTED))\
-    .add_edge(Edge(a,c,EdgeType.DIRECTED))\
-    .add_edge(Edge(b,d,EdgeType.DIRECTED))\
-    .add_edge(Edge(c,e,EdgeType.DIRECTED))\
-    .add_edge(Edge(d,f,EdgeType.DIRECTED))\
-    .add_edge(Edge(e,f,EdgeType.DIRECTED))\
-    .add_edge(Edge(c,g,EdgeType.DIRECTED))\
-    .add_edge(Edge(e,h,EdgeType.DIRECTED))\
-    .add_edge(Edge(g,h,EdgeType.DIRECTED))\
+from bayes import Clique,Separator
+from JoinTree import JunctionTree
+from pgmpy.factors.discrete.CPD import TabularCPD,DiscreteFactor
+import numpy as np
 
-jt = JTNode("BC",[b,c])
-p = Potential()
-ff = b.id
-b.potential = PotentialUtil.get_potential(b,Bbn.get_parents(ff))
+r = TabularCPD("r",2,[[0.8],[0.2]])
+s = TabularCPD("s",2,[[0.9],[0.1]])
+w = TabularCPD('w',2,[[0.8,0],[0.2,1]],['r'],[2])
+h = TabularCPD('h', 2,[[1,0.1,0,0],[0,0.9,1,1]],['r', 's'], [2, 2])
+print(r)
+print(s)
+print(w)
+print(h)
+ff = TabularCPD('h',2,[[0],[1]])
+ff2 = TabularCPD('w',2,[[0],[1]])
+print(ff)
+wr = Clique([w,r])
+hrs = Clique([h,r,s])
+wr.init_cpt()
+hrs.init_cpt()
+print(hrs.cpt.marginalize(['r','h'],False))
+jt = JunctionTree('f',wr,[wr,hrs])
+jt.add_separator(wr,hrs)
+jt.enter_evidence('h',1)
+jt.enter_evidence('w',1)
 
-jt.init_potential(b)
-print(jt.potential)
+##TO DO: scelta root,riguardare aggiunta vicini,e struttura generale
+print(hrs.cpt.marginalize(['r','h'],False))
 
 
-join_tree = InferenceController.apply(bbn)
-
-ev = EvidenceBuilder().with_node(join_tree.get_bbn_node_by_name('A')).with_evidence('ON',1.0).build()
-join_tree.set_observation(ev)
-for node in join_tree.get_bbn_nodes():
-    potential = join_tree.get_bbn_potential(node)
-    print(potential)
-    print('\n')
-    
