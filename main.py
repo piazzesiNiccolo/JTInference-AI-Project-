d546b845b6ed041dc8df7ce04ca5788fed416959
Niccolò Piazzesi <npiazzesi@gmail.com>
2020-03-06 15:13:33 +0100

    test

--- main.py
+++ main.py
@@ -4,37 +4,34 @@ from JoinTree import JunctionTree
 from pgmpy.factors.discrete.CPD import TabularCPD,DiscreteFactor
 import numpy as np
 
-##WET GRASS
-r = TabularCPD("r",2,[[0.8],[0.2]])
-s = TabularCPD("s",2,[[0.9],[0.1]])
-w = TabularCPD('w',2,[[0.8,0],[0.2,1]],['r'],[2])
-h = TabularCPD('h', 2,[[1,0.1,0,0],[0,0.9,1,1]],['r', 's'], [2, 2])
-wr = Clique([w,r])
-hrs = Clique([h,r,s])
-print(hrs.cpt.marginalize(['r','h'],False))
-jt = JunctionTree('f',wr,[wr,hrs])
-jt.add_separator(wr,hrs)
-jt.enter_evidence('h',1)
-jt.enter_evidence('w',1)
+#cancwe_neapolitan
+Metastatic_Cancer = TabularCPD('m',2,[[0.8],[0.2]])
+Serum_Calcium = TabularCPD('s',2,[[0.8,0.2],[0.2,0.8]],['m'],[2])
+Brain_Tumor =  TabularCPD('b',2,[[0.95,0.8],[0.05,0.2]],['m'],[2])
+Coma =  TabularCPD('c', 2,[[0.95,0.1,0.3,0.2],[0.05,0.9,0.7,0.8]],['s', 'b'], [2, 2])
+Severe_Headache = TabularCPD('h', 2,[[0.4,0.2,],[0.6,0.8]],['b'], [2])
+bsm = Clique([Metastatic_Cancer,Serum_Calcium,Brain_Tumor])
+sbc = Clique([Serum_Calcium,Brain_Tumor,Coma])
+bh = Clique([Brain_Tumor,Severe_Headache])
 
-##TO DO: scelta root,riguardare aggiunta vicini,e struttura generale
-print(hrs.cpt.marginalize(['r','h'],False))
-
+jt = JunctionTree('tree',bsm,[bsm,sbc,bh])
+jt.add_separator(bsm,sbc)
+jt.add_separator(bsm,bh)
+jt.enter_evidence('s',0)
+print(bh.cpt.marginalize(['b'],False))
 
 ##ICY ROAD
 
 icy = TabularCPD('i',2,[[0.3],[0.7]])
 holmes = TabularCPD('h',2,[[0.9,0.2],[0.1,0.8]],['i'],[2])
 watson = TabularCPD('w',2,[[0.9,0.2],[0.1,0.8]],['i'],[2])
-print(icy)
-print(holmes)
-print(watson)
 
 iw = Clique([icy,watson])
 ih = Clique([icy,holmes])
 
 jt2 = JunctionTree('t',iw,[iw,ih])
 jt2.add_separator(iw,ih)
-jt2.enter_evidence('w',0)
-print(ih.cpt.marginalize(['h'],False))
-print(iw.cpt.marginalize(['w'],False))
+jt2.enter_evidence('i',1)
+
+print(ih.cpt.marginalize(['i'],False))
+print(iw.cpt.marginalize(['i'],False))
