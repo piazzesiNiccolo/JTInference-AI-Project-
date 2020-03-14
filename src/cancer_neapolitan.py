eebe50c6ae769105ccb00fd31e13c5cb2d72d7d6
Niccolò Piazzesi <npiazzesi@gmail.com>
2020-03-14 21:20:06 +0100

    TO DO STUD FARM EXAMPLE

--- src/cancer_neapolitan.py
+++ src/cancer_neapolitan.py
@@ -11,7 +11,6 @@ Serum_Calcium = TabularCPD('s',2,[[0.8,0.2],[0.2,0.8]],['m'],[2])
 Brain_Tumor =  TabularCPD('b',2,[[0.95,0.8],[0.05,0.2]],['m'],[2])
 Coma =  TabularCPD('c', 2,[[0.95,0.1,0.3,0.2],[0.05,0.9,0.7,0.8]],['b','s'], [2, 2])
 Severe_Headache = TabularCPD('h', 2,[[0.4,0.2,],[0.6,0.8]],['b'], [2])
-
 print('setting up junction tree...')
 bsm = Clique([Metastatic_Cancer,Serum_Calcium,Brain_Tumor])
 sbc = Clique([Serum_Calcium,Brain_Tumor,Coma])
---
+++ src/studfarm.py
@@ -0,0 +1,4 @@
+from tree.node import Clique,Separator
+from tree.junction_tree import JunctionTree
+from pgmpy.factors.discrete.CPD import TabularCPD
+
