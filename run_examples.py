895d84b6522962fb57d3d7b274a248af1282c33a
Niccolò Piazzesi <npiazzesi@gmail.com>
2020-03-23 15:10:48 +0100

    base version done,TO DO improve readibility

--- run_examples.py
+++ run_examples.py
@@ -3,12 +3,35 @@ import examples.icy_roads as ice
 import examples.fire as fire
 import examples.cancer_neapolitan as cancer
 import examples.mrs_gibbon as mrs
+print('#############################################################################################\n\n')
+print('This simple program test the HUGIN algorithm for belief propagation in junction trees\n\n')
+print('To do so, it creates 4 examples of bayesian networks and their junction tree\n')
+print('On each network it  runs a certain amount of queries\n')
+print('For each query it shows the wanted probability before and after entering some kind of evidence\n')
+
+print('\n\n#############################################################################################')
+input('PRESS ANY KEY TO START THE PROGRAM')
 
 examples = {1:ice.icy_roads_example, 2:mrs.mrs_gibbon_example, 3:fire.fire_example,4:cancer.cancer_neapolitan_example}
-choice = ''
-while choice !=  '0': 
-    choice = input('scegli: ').lower()
-    ex = int(choice)
-    examples.get(ex, lambda: print('invalid choice'))()
+
+
+while True: 
+    choice = input('select a number to run an example:\n\
+1: icy_roads\n\
+2: mrs_gibbon\n\
+3: fire\n\
+4: cancer_neapolitan\n\
+quit: EXIT PROGRAM\n\n').lower()
+    if choice == 'quit':
+        break
+    try:
+        ex = int(choice)
+        run = examples.get(ex, lambda: print('invalid choice, choose a number between 1 and 4'))
+        run()
+    except ValueError:
+        print('incorrect value, try again')
+        
+        
+    
     
 
