52293cc2084b97f3ebb17d6dbd9d0d8e591e6e0b
Niccolò Piazzesi <npiazzesi@gmail.com>
2020-03-28 14:26:44 +0100

    all examples are in english

--- run_examples.py
+++ run_examples.py
@@ -11,18 +11,14 @@ print('On each network it  runs a certain amount of queries\n')
 print('For each query it shows the wanted probability before and after entering some kind of evidence\n')
 
 print('\n\n#############################################################################################')
-input('PRESS ANY KEY TO START THE PROGRAM\n\n\n')
+input('PRESS ENTER TO START THE PROGRAM\n\n\n')
 
 examples = {1:ice.icy_roads_example, 2:mrs.mrs_gibbon_example, 3:fire.fire_example,4:cancer.cancer_neapolitan_example}
 
 exit = False
 while not exit: 
-    choice = input('select a number to run an example:\n\
-1: icy_roads\n\
-2: mrs_gibbon\n\
-3: fire\n\
-4: cancer_neapolitan\n\
-quit: EXIT PROGRAM\n\n:').lower()
+    choice = input('\nSelect a number to run an example:\n\n\
+1: icy_roads   2: mrs_gibbon   3: fire  4: cancer_neapolitan  quit: EXIT PROGRAM\n\n:').lower()
     
     if choice == 'quit':
         exit = True
@@ -31,9 +27,9 @@ quit: EXIT PROGRAM\n\n:').lower()
         ex = int(choice)
         run = examples.get(ex, lambda: print('invalid choice, choose a number between 1 and 4'))
         run()
-        exit = input('do you want to continue? [Y/n]').lower() == 'n' 
     except ValueError:
         print('\nINCORRECT VALUE, TRY AGAIN\n')
+        
         
         
     
